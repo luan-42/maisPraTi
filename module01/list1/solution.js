6b2fc050c3ccd3ae7c330282f4a9378da2b11afb
Luan Ferreira <178341285+luan-42@users.noreply.github.com>
2024-08-19 16:50:57 -0300

    exercícios 3-4

--- module01/list1/solution.js
+++ module01/list1/solution.js
@@ -4,8 +4,16 @@ function redColor(msg) {
     return `\x1b[31m${msg}\x1b[0m`;
 }
 
+function greenColor(msg) {
+    return `\x1b[32m${msg}\x1b[0m`;
+}
+
 function yellowColor(msg) {
     return `\x1b[33m${msg}\x1b[0m`;
+}
+
+function blueColor(msg) {
+    return `\x1b[34m${msg}\x1b[0m`;
 }
 
 //Lê apenas número do usuário
@@ -40,4 +48,43 @@ function classifyAge() {
     else
         result = "idoso";
     console.log(`Com ${yellowColor(age)} anos é considerado ${result}`);
+}
+
+//3. Implemente um programa que recebe uma nota de 0 a 10 e classifica como "Aprovado", "Recuperação", ou "Reprovado" utilizando if-else if.
+function studentResult() {
+    let grade = readNumber("Digite a nota do estudante: ");
+    while(!(grade >= 0 && grade <= 10)) {
+        console.log(yellowColor("A nota deve estar entre 0 e 10!"));
+        grade = readNumber("Digite a nota do estudante: ");
+    }
+    let result;
+    if (grade < 6)
+        result = "Reprovado";
+    else if (grade < 7)
+        result = "Recuperação";
+    else
+        result = "Aprovado";
+    console.log(`Com a nota ${yellowColor(grade)} a situação do aluno é de ${result}`);
+}
+
+//4. Crie um menu interativo no console que oferece ao usuário a escolha de três opções. Utilize switch-case para implementar a lógica de cada opção selecionada.
+function menu() {
+    console.log("Escolha a cor da mensagem: \n1 - Verde \n2 - Amarelo \n3 - Azul");
+    let color = readNumber("Escolha: ");
+    while(!(color >= 1 && color <= 3)) {
+        console.log(redColor("Escolha deve ser entre 1 e 3!"));
+        color = readNumber("Escolha: ");
+    }
+    let msg = "Olá, Mundo!";
+    switch (color) {
+        case 1:
+            console.log(greenColor(msg));
+            break;
+        case 2:
+            console.log(yellowColor(msg));
+            break;
+        case 3:
+            console.log(blueColor(msg));
+            break;
+    }
 }
